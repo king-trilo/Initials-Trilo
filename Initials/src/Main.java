08ed3bd9a238ef6ff8defbbab87ab20059f09a18
king-trilo <trilo.thilak@gmail.com>
2025-08-12 08:53:39 -0700

    version_completed_faster_d6

--- Initials/src/Main.java
+++ Initials/src/Main.java
@@ -3,6 +3,7 @@ import kareltherobot.*;
 public class Main implements Directions{
   public static void main(String[] args) {
     World.setVisible(true);// allows us to see the run
+    World.setDelay(6);
 
     // The line below creates a Robot that we will refer to as r.  
     // Find out what the numbers and direction do!
